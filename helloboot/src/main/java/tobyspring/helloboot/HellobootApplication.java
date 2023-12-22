46d8716f7bbcb88bebf5d17d55cac1abc12d001a
ChaeJeong <pagw1604@gmail.com>
2023-12-22 13:59:38 +0900

    03. 프론트 컨트롤러로 전환

--- helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,16 +1,14 @@
 package tobyspring.helloboot;
 
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -22,17 +20,31 @@ public class HellobootApplication {
 	public static void main(String[] args) {
 		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			servletContext.addServlet("hello", new HttpServlet() {
+			HelloController helloController = new HelloController();
+
+			servletContext.addServlet("frontcontroller", new HttpServlet() {
 				@Override
 				public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-					String name = req.getParameter("name");
+					// 인증, 보안, 다국어, 공통 기능
+					if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())){
+						String name = req.getParameter("name");
 
-					res.setStatus(HttpStatus.OK.value());
-					res.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-					res.getWriter().println("s:Hello "+ name);
+						String ret = helloController.hello(name);
+
+						res.setStatus(HttpStatus.OK.value());
+						res.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+						res.getWriter().println(ret);
+					}
+					else if (req.getRequestURI().equals("/user")){
+						//
+						res.setStatus(HttpStatus.NOT_FOUND.value());
+					}
+					else {
+						res.setStatus(HttpStatus.NOT_FOUND.value());
+					}
 
 				}
-			}).addMapping("/hello");
+			}).addMapping("/*");
 		});
 		webServer.start();
 
