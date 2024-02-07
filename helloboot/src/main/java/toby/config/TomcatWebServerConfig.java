f8102e29234bca683face157e5c849d4674be3a7
ChaeJeong <pagw1604@gmail.com>
2024-02-07 16:11:20 +0900

    07. 조건부 자동 구성 - Tomcat 서버, Jetty 서버 핸들링 코드 추가

--- helloboot/src/main/java/toby/config/TomcatWebServerConfig.java
+++ helloboot/src/main/java/toby/config/TomcatWebServerConfig.java
@@ -3,14 +3,27 @@ package toby.config;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.core.type.AnnotatedTypeMetadata;
 import toby.annotation.MyAutoConfiguration;
 
 @MyAutoConfiguration
+@Conditional(TomcatWebServerConfig.TomcatCondition.class)
 public class TomcatWebServerConfig {
-    @Bean
+    @Bean("tomcatServletWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory(){
-        var tomcatServletWebServerFactory = new TomcatServletWebServerFactory();
-        tomcatServletWebServerFactory.setPort(8990);
-        return tomcatServletWebServerFactory;
+        var webServerFactory = new TomcatServletWebServerFactory();
+        webServerFactory.setPort(8990);
+        return webServerFactory;
+    }
+
+    static class TomcatCondition implements Condition {
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            return false;
+        }
     }
 }
+
