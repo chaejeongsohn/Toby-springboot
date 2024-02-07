73f161ca58780286e968bb2c40113e070252c6db
ChaeJeong <pagw1604@gmail.com>
2024-02-07 17:41:21 +0900

    코드 정리 - 주석 정리 - import 정리

--- helloboot/src/main/java/toby/config/JettyWebServerConfig.java
+++ helloboot/src/main/java/toby/config/JettyWebServerConfig.java
@@ -10,16 +10,14 @@ import org.springframework.core.type.AnnotatedTypeMetadata;
 import toby.annotation.MyAutoConfiguration;
 
 /**
- * @ Conditional
- *      : Configuration 중에서 어떤 경우에 적용이 되고 적용되지 않는가를 선택하게 만드는 방법을 추가해야한다.
- *      Element Type : 인터페이스 Condition 을 구현한 class
- *
- *      ConditionContext
+ * @Conditional
+ *      Configuration 중에서 어떤 경우에 적용이 되고 적용되지 않는가를 선택하게 만드는 방법을 추가해야한다.
+ *      - Element Type : 인터페이스 Condition 을 구현한 class
+ *                      JettyWebServerConfig.JettyCondition.class
+ *      - ConditionContext
  *          현재 Spring Container 와 Application 이 돌아가는 환경 정보에 관한 Object 리턴해준다.
- *      AnnotatedTypeMetadata
- *          Conditional 이 붙은 해당 Meta-Annotation 으로 사용하는
- *          다른 Annotation 정보들을 이용할 수 있도록
- *          Annotation 관련 Meta 데이터 리턴해준다.
+ *      - AnnotatedTypeMetadata
+ *          @Conditional 이 붙은 Annotation 을 Meta-Annotation 으로 사용하는 Annotation 의 Meta 데이터를 가져온다.
  */
 @MyAutoConfiguration
 @Conditional(JettyWebServerConfig.JettyCondition.class)
--- helloboot/src/test/java/toby/study/ConfigurationTest.java
+++ helloboot/src/test/java/toby/study/ConfigurationTest.java
@@ -1,12 +1,11 @@
 package toby.study;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.setRemoveAssertJRelatedElementsFromStackTrace;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ConfigurationTest {
     @Test
